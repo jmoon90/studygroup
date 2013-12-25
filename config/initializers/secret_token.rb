71e90b534d168d217f3c3506209532569606afca
John Moon <johnmoonyy@gmail.com>
2013-12-25 12:12:12 -0500

    initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Studygroup::Application.config.secret_key_base = 'd41cab9cfeea153e6db5f16700041135ca4727419ed3a7abeb98991c01082ab639ccd9d15db9d982baf94d7db74fbcca30c7ce7d2be07bcc19f3b4e69e9b8647'
+Studygroup::Application.config.secret_key_base = SECRET_KEY=thisisarandomstringhopefullylongerthan40characters
