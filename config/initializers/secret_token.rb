572a8866b20a630f46fecfe7355f71728fd16c90
John Moon <johnmoonyy@gmail.com>
2013-12-25 12:28:51 -0500

    intial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Studygroup::Application.config.secret_key_base = SECRET_KEY=thisisarandomstringhopefullylongerthan40characters
+Studygroup::Application.config.secret_key_base = if Rails.env.development? or Rails.env.test?
+    ('x' * 30)
+else
+    ENV['SECRET_TOKEN']
+end
