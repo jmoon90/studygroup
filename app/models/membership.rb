3af8ed73fe9519a9af1e15e45868d9c06599ed09
John Moon <johnmoonyy@gmail.com>
2013-12-27 15:19:55 -0500

    Seed user and group id into membership

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,6 +1,15 @@
 class Membership < ActiveRecord::Base
   belongs_to :user,
-    inverse_of: :memerships
+    inverse_of: :memberships
   belongs_to :group,
-    inverse_of: :memerships
+    inverse_of: :memberships
+
+  def self.seeder(group)
+    group.each do |group|
+      Membership.find_or_initialize_by(group_id: group[:group_id]) do |mem|
+        mem.user_id = group[:user_id]
+        mem.save
+      end
+    end
+  end
 end
--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -6,4 +6,18 @@ describe Membership do
 
   it { should have_valid(:user_id).when(User.new) }
   it { should have_valid(:group_id).when(Group.new) }
+
+  describe "#seeder" do
+    it "joins user and group table" do
+      group = [{group_id: 1}]
+      Membership.seeder(group)
+      expect(Membership.all).should_not be_empty
+    end
+    it "joins user and group table" do
+      user = [{user_id: 1}]
+      Membership.seeder(user)
+      expect(Membership.all).should_not be_empty
+    end
+
+  end
 end
