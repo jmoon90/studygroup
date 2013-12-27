11da5427e83d72506fcee7f074620e239db7a045
John Moon <johnmoonyy@gmail.com>
2013-12-27 12:59:38 -0500

    Fix case insensitive validation for tutorial title

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,4 +1,5 @@
 class Tutorial < ActiveRecord::Base
+  validates_uniqueness_of :name, case_sensitive: false
   validates_presence_of :name
   has_many :groups,
     inverse_of: :tutorial
--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe Tutorial do
-  it { should validate_presence_of :name }
+  it { should validate_presence_of(:name) }
+
+  it do
+    should validate_uniqueness_of(:name).case_insensitive
+  end
+
   it { should have_many :groups }
 end
