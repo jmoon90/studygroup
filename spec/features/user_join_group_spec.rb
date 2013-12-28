3188a752c1479dffae7f90809788d466e1389586
John Moon <johnmoonyy@gmail.com>
2013-12-28 13:26:03 -0500

    Refactor user_join_group_spec

--- spec/features/user_join_group_spec.rb
+++ spec/features/user_join_group_spec.rb
@@ -1,36 +1,32 @@
 require 'spec_helper'
 
 feature 'Join group' do
+  given!(:group) { FactoryGirl.create(:group) }
   scenario 'join and leave present' do
-    FactoryGirl.create(:group)
     visit root_path
     first(:link, 'Hartl').click
     expect(page).to have_content('join')
   end
 
   scenario 'join' do
-    FactoryGirl.create(:group)
-    FactoryGirl.create(:membership)
     user = FactoryGirl.create(:user)
 
-    visit root_path
-    first(:link, 'Hartl').click
-    log_in_user(user)
-    first(:link, 'join').click
+    sign_in(user)
     expect(page).to have_content(user.email)
   end
 
   scenario 'leave' do
-    FactoryGirl.create(:group)
-    FactoryGirl.create(:membership)
     user = FactoryGirl.create(:user)
+    sign_in(user)
 
-    visit root_path
-    first(:link, 'Hartl').click
-    log_in_user(user)
-    first(:link, 'join').click
     first(:link, 'leave').click
     expect(page).to_not have_content(user.email)
   end
 
+  def sign_in(user)
+    visit root_path
+    first(:link, 'Hartl').click
+    log_in_user(user)
+    first(:link, 'join').click
+  end
 end
