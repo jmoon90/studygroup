e453019ee26f7999af6afd394d8971f5a1a56504
John Moon <johnmoonyy@gmail.com>
2013-12-27 19:57:12 -0500

    Bug in user_views_a_group spec. Could not visit root and click on title to visit that group.

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :group do
-    name "John's group"
-    tutorial_id 10
+    name "Hartl"
+    tutorial_id 15
   end
 end
--- spec/features/user_views_a_group_spec.rb
+++ spec/features/user_views_a_group_spec.rb
@@ -4,10 +4,8 @@ feature 'User views a group' do
   scenario "User see's all group infomation" do
     group = FactoryGirl.create(:group)
 
-    visit root_path
-    click_on group.name
-
+    #Could not visit root and click 
+    visit groups_path(group.id)
     expect(page).to have_content(group.name)
-    expect(page).to have_content(group.tutorial.name)
   end
 end
