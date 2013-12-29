6ccc86d1b8988c3fd3015c703f63f940ba25b690
John Moon <johnmoonyy@gmail.com>
2013-12-28 19:37:33 -0500

    Delete empty lines

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -1,16 +1,18 @@
 require 'spec_helper'
 
 feature 'Sign up' do
-  before(:each) { visit new_user_registration_path }
-  scenario 'with valid information' do
+  before(:each) do
+    visit new_user_registration_path
+  end
 
+  scenario 'with valid information' do
     within('form#new_user') do
       fill_in 'Email', with: 'jayleno@aol.com'
       fill_in 'Password', with: 'applepie'
       fill_in 'Password confirmation', with: 'applepie'
-
       click_button 'Sign up'
     end
+
     expect(page).to have_content('signed up successfully')
   end
 
@@ -19,6 +21,4 @@ feature 'Sign up' do
 
     expect(page).to have_content('error')
   end
-
-
 end
