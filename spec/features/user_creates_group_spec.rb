c8e5149829d42084d4a624385b3a266a819a1275
John Moon <johnmoonyy@gmail.com>
2013-12-28 19:00:42 -0500

    Refactor duplicate code user_creates_group

--- spec/features/user_creates_group_spec.rb
+++ spec/features/user_creates_group_spec.rb
@@ -1,19 +1,20 @@
 require 'spec_helper'
 
 feature Group do
-  scenario 'user can create new group' do
+  before :each do
     visit new_group_path
-    expect(page).to have_selector(:link_or_button, 'Create group')
+  end
+
+  scenario 'user can create new group' do
+    page.should have_selector(:link_or_button, 'Create group')
   end
 
   scenario 'user can select a tutorial' do
-    visit new_group_path
-    expect(page).to have_content('Please select')
+    page.should have_content('Please select')
   end
 
   scenario 'invalid input' do
-    visit new_group_path
     click_button 'Create group'
-    expect(page).to have_content("can't be blank")
+    page.should have_content("can't be blank")
   end
 end
