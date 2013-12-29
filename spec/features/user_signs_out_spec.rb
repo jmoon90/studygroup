1c0582ddcba473589881c7aaeead809d1e9412c6
John Moon <johnmoonyy@gmail.com>
2013-12-28 19:32:17 -0500

    Refactor before block in user_signs_out and add new test

--- spec/features/user_signs_out_spec.rb
+++ spec/features/user_signs_out_spec.rb
@@ -2,25 +2,17 @@ require 'spec_helper'
 
 feature 'Sign out' do
   before(:each) do
-    FactoryGirl.create(:user)
-    visit new_user_session_path
-
-
-    fill_in "Email", with: 'j@aol.com'
-    fill_in "Password", with: 'applepie'
-
-    click_button 'Sign in'
+    user = FactoryGirl.create(:user)
+    log_in_user(user)
+    visit root_path
   end
 
   scenario 'when signed in' do
     expect(page).to have_content ('Sign out')
   end
 
-  #scenario 'the user' do
-  #    within('.sign_out_user') do
-  #      click_link "Sign out"
-  #    end
-  #  expect(page).to have_content ('Are you sure?')
-  #end
-
+  scenario 'the user' do
+    click_link "Sign out"
+    expect(page).to have_content ('Sign In')
+  end
 end
