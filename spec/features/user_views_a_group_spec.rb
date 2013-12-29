4dfb2f39d716c1de0990d1bf4f6b1c33f4fa9447
John Moon <johnmoonyy@gmail.com>
2013-12-28 19:54:53 -0500

    Refactor user_views_a_group by reducing duplication from scenario and moving it to before each. Added test to see user email if they join a group

--- spec/features/user_views_a_group_spec.rb
+++ spec/features/user_views_a_group_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 feature 'User views a group' do
-  scenario "User see's all group infomation" do
-    group = FactoryGirl.create(:group)
+  given(:user) { FactoryGirl.create(:user) }
+  before :each do
+    FactoryGirl.create(:tutorial)
+    FactoryGirl.create(:group)
+    visit root_path
+    first(:link, 'Hartl').click
+  end
 
-    #Could not visit root and click 
-    visit groups_path(group.id)
-    expect(page).to have_content(group.name)
+  scenario "User see's all group infomation" do
+    expect(page).to have_content("Hartl")
+  end
+
+  scenario "User see's other users in group" do
+    log_in_user(user)
+    click_on "join"
+    expect(page).to have_content(user.email)
   end
 end
