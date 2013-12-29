313ce3c58a8a230af3077a94648c7f977d8a8d02
John Moon <johnmoonyy@gmail.com>
2013-12-28 19:13:34 -0500

    Refactor user_edit_info of duplicate code

--- spec/features/user_edits_info_spec.rb
+++ spec/features/user_edits_info_spec.rb
@@ -1,35 +1,36 @@
 require 'spec_helper'
 
 feature 'Edit personal information' do
-  before(:each) do
-    visit new_user_session_path
-    FactoryGirl.create(:user)
-    fill_in 'Email', with: 'j@aol.com'
-    fill_in 'Password', with: 'applepie'
+  given(:user) { FactoryGirl.create(:user) }
+  given(:password_fill_in) {
+    fill_in 'Password', with: 'cherrypie'
+    fill_in 'Password confirmation', with: 'cherrypie'
+  }
+  given(:update_button) { click_button 'Update' }
 
-    click_button 'Sign in'
+  before(:each) do
+    log_in_user(user)
     visit edit_user_registration_path
   end
 
   scenario 'password change' do
-    fill_in 'Password', with: 'cherrypie'
-    fill_in 'Password confirmation', with: 'cherrypie'
+    password_fill_in
     fill_in 'Current password', with: 'applepie'
-
-    click_button 'Update'
+    update_button
 
     expect(page).to have_content('You updated your account successfully')
   end
 
+  scenario 'incorrect current password' do
+    password_fill_in
+    update_button
+
+    expect(page).to have_content("Current password can't be blank")
+  end
+
   scenario 'cancel account' do
     click_button 'Cancel my account'
-
-    visit new_user_session_path
-    fill_in 'Email', with: 'j@aol.com'
-    fill_in 'Password', with: 'applepie'
-
-    click_button 'Sign in'
-    expect(page).to have_content('Invalid email or password')
+    expect(page).to have_content('Bye!')
   end
 end
 
