62fcd5d05c978280233e8dd5c6d1038f4650420c
John Moon <johnmoonyy@gmail.com>
2013-12-30 14:42:05 -0500

    Fix user sign in spec

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 feature 'Sign in' do
+    given(:user) { FactoryGirl.create(:user) }
   before(:each) do
     visit new_user_session_path
-    FactoryGirl.create(:user)
   end
 
   scenario 'with valid information' do
-    fill_in 'Email', with: 'j@aol.com'
-    fill_in 'Password', with: 'applepie'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
     click_button 'Sign in'
 
     expect(page).to have_content('Signed in successfully')
