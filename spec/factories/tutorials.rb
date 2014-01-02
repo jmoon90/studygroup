a5b2a23429c239596fe040111b6107105eee1ab1
John Moon <johnmoonyy@gmail.com>
2014-01-02 17:10:29 -0500

    Add test to see if image was being uploaded properly

--- spec/factories/tutorials.rb
+++ spec/factories/tutorials.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :tutorial do
     name "Rail Tutorial"
+    image "http://railstutorial.org/images/layout/logo.png"
   end
 end
--- spec/features/admin_creates_tutorial_spec.rb
+++ spec/features/admin_creates_tutorial_spec.rb
@@ -4,9 +4,13 @@ feature "Tutorial list" do
   before(:each) do
     visit new_tutorial_path
   end
+   given(:tutorial) { FactoryGirl.create(:tutorial) }
+   given(:local_path) { File.join(Rails.root, '/spec/support/example.png') }
 
   scenario 'valid inputs' do
+
     fill_in "Name", with: 'BoogyRails'
+    attach_file('tutorial_image', local_path)
     click_button 'Add Tutorial'
 
     expect(page).to have_content("Added new tutorial")
