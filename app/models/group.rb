f56da9f33f39916852ccb5bced1c7b3a46d2c2b8
John Moon <johnmoonyy@gmail.com>
2014-01-03 14:14:45 -0500

    Add feature to upload images to tutorials. Display the image when user creates the specific tutorial.
    NEED to fix sort

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,11 +1,4 @@
 class Group < ActiveRecord::Base
- # WHY DOESN"T attr accessor work?
-  attr_writer :args
-
-  def self.args
-    @args ||= Hash.new
-  end
-
   validates_presence_of :tutorial_id
   validates_presence_of :name
   validates_presence_of :size
@@ -25,20 +18,11 @@ class Group < ActiveRecord::Base
   has_many :posts,
     inverse_of: :group
 
-  def self.finds_max_size(group)
-    self.args
-    self.max_group_size(group)
-    self.current_group_size(group)
+  def self.acs
+    order(group.users :acs)
   end
 
-  def self.max_group_size(group)
-    @args[:group_max] = group.size
-    @args
-  end
-
-  #Design decision to separte two code that are very similar.
-  def self.current_group_size(group)
-    @args[:current_group_size] = group.users.count
-    @args
+  def self.desc
+    order(group.users :desc)
   end
 end
--- spec/features/user_views_home_page_spec.rb
+++ spec/features/user_views_home_page_spec.rb
@@ -1,15 +1,21 @@
 require 'spec_helper'
 
 feature 'user visit home page' do
-  given(:group) { FactoryGirl.create(:group) }
+  given(:group) { FactoryGirl.create(:group, name: 'hartl') }
 
   scenario 'sort not full groups to the top' do
+    tutorial = FactoryGirl.create(:tutorial, name: 'mu', id: 16, image: nil)
+    group1 = FactoryGirl.create(:group, name: 'monsu', id: 16, tutorial_id: tutorial.id)
     FactoryGirl.create(:membership, group: group)
     FactoryGirl.create(:membership, group: group)
+    FactoryGirl.create(:membership, group: group)
+    FactoryGirl.create(:membership, group: group1)
 
     visit root_path
-    click_on sort_unfull
+#    save_and_open_page
+    click_on 'sort by size'
 
+    page.should have_tag("li:first-child", text: 'hartl')
   end
 
   scenario 'view groups current member size against max memeber size' do
