53fb801ca4ffbc479250855d243f55170e04d8e7
John Moon <johnmoonyy@gmail.com>
2014-01-05 14:25:42 -0500

    Add test sort button in group index page.

--- spec/features/user_views_home_page_spec.rb
+++ spec/features/user_views_home_page_spec.rb
@@ -3,28 +3,47 @@ require 'spec_helper'
 feature 'user visit home page' do
   given(:group) { FactoryGirl.create(:group, name: 'hartl') }
 
-  scenario 'sort not full groups to the top' do
+  scenario 'sort by most amount of users' do
     tutorial = FactoryGirl.create(:tutorial, name: 'mu', id: 16, image: nil)
 
     group1 = FactoryGirl.create(:group, name: 'monsu', id: 16, tutorial_id: tutorial.id)
-    3.times do
+    2.times do
       FactoryGirl.create(:membership, group: group)
     end
     FactoryGirl.create(:membership, group: group1)
 
     visit root_path
-    click_on 'Sort by size'
+    within(".f-dropdown") do
+      click_on 'Most Users'
+    end
 
-    page.should have_tag("li:first-child", text: 'hartl')
+    page.body.index('hartl').should < page.body.index('monsu')
+  end
+
+  scenario 'sort by least amount of users' do
+    tutorial = FactoryGirl.create(:tutorial, name: 'mu', id: 16, image: nil)
+    group1 = FactoryGirl.create(:group, name: 'monsu', id: 16, tutorial_id: tutorial.id)
+
+    2.times do
+      FactoryGirl.create(:membership, group: group)
+    end
+    FactoryGirl.create(:membership, group: group1)
+
+    visit root_path
+    within(".f-dropdown") do
+      click_on 'Least Users'
+    end
+
+    page.body.index('monsu').should < page.body.index('hartl')
   end
 
   scenario 'view groups current member size against max memeber size' do
-    3.times do
+    2.times do
       FactoryGirl.create(:membership, group: group)
     end
     visit root_path
 
-    expect(page).to have_content('3 / 12')
+    expect(page).to have_content('2 / 12')
   end
 
   scenario 'sort full groups to the top' do
