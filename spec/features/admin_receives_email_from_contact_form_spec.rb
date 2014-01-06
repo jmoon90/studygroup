b32826f773b9083e764641a7a58d7657b2a5b332
John Moon <johnmoonyy@gmail.com>
2014-01-06 11:32:44 -0500

    Add spec features for the contact page from users perspective

---
+++ spec/features/admin_receives_email_from_contact_form_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature 'owner receives email' do
+end
+
+As a site staff member
+I want to receive contact form inquiries via email
+So that I can see visitor feedback
+Acceptance Criteria:
+
+The dispatched email should come from the visitor's email address
+The dispatched email should include the visitor's first and last name
+The dispatched email should content description information submitted
---
+++ spec/features/user_contact_admin_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+#As a site visitor
+#I want to contact the site's staff
+#So that I can ask questions or make comments about the site
+
+#Acceptance Criteria:
+
+#I must specify a valid email address
+#I must specify a subject
+#I must specify a description
+#I must specify a first name
+#I must specify a last name
+
+
+feature 'site visitor wants to contact site staff' do
+  scenario 'fills in form with valid information' do
+    visit contact_path
+    fill_in 'Email', with: 'random@gmail.com'
+    fill_in 'Subject', with: 'How do you make money?'
+    fill_in 'Description', with: 'Love the product, but how do you make money'
+    fill_in 'First Name', with: 'random'
+    fill_in 'Last Name', with: 'person'
+    click_on 'Submit'
+
+    expect(page).to have_content('Your message has been sent.')
+  end
+
+  scenario 'fills in form with invalid information' do
+    visit contact_path
+    click_on 'Submit'
+
+    expect(page).to have_content('Invalid information. Please try again.')
+  end
+end
