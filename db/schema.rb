b7f19879db4917f392136261d959bfe9b8d30e1f
John Moon <johnmoonyy@gmail.com>
2014-01-14 19:43:39 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140111041504) do
+ActiveRecord::Schema.define(version: 20140114213817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 20140111041504) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 
   create_table "taggings", force: true do |t|
     t.integer  "taggable_id",   null: false
