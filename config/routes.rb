cdcd5999a42742ec44cbb9a40073921313288217
John Moon <johnmoonyy@gmail.com>
2014-01-20 11:06:42 -0500

    Add my learning page controller view and route

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Studygroup::Application.routes.draw do
   resources :messages, only: [:new,:create]
 
   resources :learnings, only: [:index, :show, :new, :create, :edit, :update] do
+    get 'mylearning'
     resources :votes, only: [:create, :destroy]
   end
 
