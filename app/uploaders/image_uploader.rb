a27a5ff0684df5c9fa6e79f79ba37ee1e87633aa
John Moon <johnmoonyy@gmail.com>
2014-01-19 22:55:11 -0500

    Add fog and set up carrier wave with aws

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,13 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
- storage :file
+  if Rails.env.production? || Rails.env.development?
+    storage :fog
+  else
+    storage :file
+  end
+
+include CarrierWave::MimeTypes
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
