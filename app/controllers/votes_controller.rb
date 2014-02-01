aeea8b99112ffce03be80f6c37711fbc18449a14
John Moon <johnmoonyy@gmail.com>
2014-02-01 18:30:25 -0500

    Change vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,10 @@
 class VotesController < ApplicationController
   def create
     learning = Learning.find(params[:learning_id])
-    @vote = learning.votes.build(user_id: current_user.id)
+    @vote = Vote.find_or_create_by(user_id: current_user.id, learning_id: learning.id)
+    Learning.rank_learning(learning)
 
     if @vote.save
-      Learning.rank_learning(learning)
       redirect_to learnings_path, notice: "We heard your hearts vote"
     else
       redirect_to learnings_path
@@ -14,9 +14,9 @@ class VotesController < ApplicationController
   def destroy
     @learning = Learning.find(params[:learning_id])
     Learning.rank_learning(@learning)
-    @vote = Vote.find(params[:id])
-    @vote.destroy
 
+    @vote = Vote.find(params[:id])
+    Vote.destroy(@vote)
     redirect_to learnings_path, notice: "We heard the swaying of your heart"
   end
 end
