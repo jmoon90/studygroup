3244451e18e237ced8cb9f3933b9d55f5cb39c78
John Moon <johnmoonyy@gmail.com>
2014-02-01 15:44:26 -0500

    Fix votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,22 +1,19 @@
 class VotesController < ApplicationController
   def create
     learning = Learning.find(params[:learning_id])
-
     @vote = current_user.votes.build()
     @vote.learning = learning
 
-
     if @vote.save
       flash[:notice] = "We heard the swaying of your heart"
-      Learning.rank_learning(learning)
+    #  Learning.rank_learning(learning)
       redirect_to learnings_path
     end
   end
 
   def destroy
-    learning = Learning.find(params[:learning_id])
-    Learning.rank_learning(learning)
-
+    #learning = Learning.find(params[:learning_id])
+    #Learning.rank_learning(learning)
     current_user.votes.delete(params[:id])
     redirect_to learnings_path
   end
