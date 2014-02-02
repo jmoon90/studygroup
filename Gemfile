67ef6f30ee98f656c898afd68a47687c70870b51
John Moon <johnmoonyy@gmail.com>
2014-02-02 15:48:08 -0500

    Add pygments and redcarpet for markdown like syntax and code highlighting.

--- Gemfile
+++ Gemfile
@@ -18,6 +18,10 @@ gem 'sass-rails', '~> 4.0.0'
 
 gem 'unicorn'
 
+gem 'redcarpet'
+
+gem 'pygments.rb'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  class HTMLwithPygments < Redcarpet::Render::HTML
+    def block_code(code)
+    #  sha = Digest::SHA1.hexdigest(code)
+    #  Rails.cache.fetch ["code", language, sha].join('-') do
+        Pygments.highlight(code)
+    #  end
+    end
+  end
+
+  def markdown(text)
+    renderer = HTMLwithPygments.new(hard_wrap: true)
+    options = {
+      autolink: true,
+      filter_html: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+      superscript: true
+    }
+    Redcarpet::Markdown.new(renderer).render(text).html_safe
+  end
 end
