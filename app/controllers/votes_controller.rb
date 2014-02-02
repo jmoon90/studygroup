824485971c434f2a7147fe258007674846d4225c
John Moon <johnmoonyy@gmail.com>
2014-02-01 22:22:27 -0500

    Fix vote create and destroy redirect_to

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,6 @@
 class VotesController < ApplicationController
   def create
-    learning = Learning.find(params[:learning_id])
-    @vote = Vote.find_or_create_by(user_id: current_user.id, learning_id: learning.id)
-    Learning.rank_learning(learning)
+    @vote = Vote.find_or_create_by(user_id: current_user.id, learning_id: params[:learning_id])
 
     if @vote.save
       redirect_to learnings_path, notice: "We heard your hearts vote"
@@ -12,9 +10,6 @@ class VotesController < ApplicationController
   end
 
   def destroy
-    @learning = Learning.find(params[:learning_id])
-    #Learning.rank_learning(@learning)
-
     @vote = Vote.find(params[:id])
     Vote.destroy(@vote)
     redirect_to groups_path, notice: "We heard the swaying of your heart"
