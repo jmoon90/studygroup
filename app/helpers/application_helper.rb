9ff96cd5ef25895527d2800743c5db818812bf9b
John Moon <johnmoonyy@gmail.com>
2014-02-06 18:16:09 -0500

    Fix syntax bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,14 +12,13 @@ module ApplicationHelper
     renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
     options = {
       autolink: true,
-      filter_html: true,
       no_intra_emphasis: true,
-      lax_html_blocks: true,
-      lax_spacing: true,
       fenced_code_blocks: true,
+      lax_html_blocks: true,
       strikethrough: true,
       superscript: true
     }
     Redcarpet::Markdown.new(renderer, options).render(text).html_safe
+
   end
 end
